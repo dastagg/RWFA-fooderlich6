bd010debf16013d0fda715e9f8a5848de69a64a3
dastagg:opus5 <dastagg@stagglinux.com>
2022-04-01 18:24:19 -0400

    Add grocery item and list views.

--- lib/screens/grocery_screen.dart
+++ lib/screens/grocery_screen.dart
@@ -3,6 +3,7 @@ import 'package:provider/provider.dart';
 
 import 'empty_grocery_screen.dart';
 import 'grocery_item_screen.dart';
+import 'grocery_list_screen.dart';
 import '../models/models.dart';
 
 class GroceryScreen extends StatelessWidget {
@@ -37,7 +38,7 @@ class GroceryScreen extends StatelessWidget {
     return Consumer<GroceryManager>(
       builder: (context, manager, child) {
         if (manager.groceryItems.isNotEmpty) {
-          return Container();
+          return GroceryListScreen(manager: manager);
         } else {
           return const EmptyGroceryScreen();
         }

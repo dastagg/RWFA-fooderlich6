91afbe57fb87c9d4d1f724c3d960e28a4147829b
dastagg:opus5 <dastagg@stagglinux.com>
2022-04-01 15:07:53 -0400

    Add GroceryManager.

--- lib/screens/grocery_screen.dart
+++ lib/screens/grocery_screen.dart
@@ -1,12 +1,38 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
 import 'empty_grocery_screen.dart';
+import '../models/models.dart';
 
 class GroceryScreen extends StatelessWidget {
   const GroceryScreen({Key? key}) : super(key: key);
 
-@override
+  @override
   Widget build(BuildContext context) {
-    return const EmptyGroceryScreen();
+    return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(Icons.add),
+        onPressed: () {
+          // TODO 11: Present GroceryItemScreen
+        },
+      ),
+      body: buildGroceryScreen(),
+    );
   }
-    // TODO: Add buildGroceryScreen
+
+  Widget buildGroceryScreen() {
+    // 1
+    return Consumer<GroceryManager>(
+      // 2
+      builder: (context, manager, child) {
+        // 3
+        if (manager.groceryItems.isNotEmpty) {
+          return Container();
+        } else {
+          // 4
+          return const EmptyGroceryScreen();
+        }
+      },
+    );
+  }
 }
